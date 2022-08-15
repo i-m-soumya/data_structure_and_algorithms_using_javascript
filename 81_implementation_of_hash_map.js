9b843b0e6925b6715aa9dff9357c778aff65c6d7
Soumya Ghosh <soumyaidmail@gmail.com>
2022-08-15 11:55:22 +0530

    Hashmap

--- 81_implementation_of_hash_map.js
+++ 81_implementation_of_hash_map.js
@@ -33,6 +33,28 @@ class HashTable {
         }
         return undefined;
     }
+
+    keys() {
+        if (!this.data.length) {
+            return undefined
+        }
+        let result = []
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+                // but also loop through all the potential collisions
+                if (this.data.length > 1) {
+                    for (let j = 0; j < this.data[i].length; j++) {
+                        result.push(this.data[i][j][0])
+                    }
+                } else {
+                    result.push(this.data[i][0])
+                }
+            }
+        }
+        return result;
+    }
 }
 
 const myHashTable = new HashTable(50);
