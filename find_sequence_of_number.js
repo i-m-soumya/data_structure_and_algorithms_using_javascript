e414badafa8334147ad4f67a84ea682664d5af8f
Soumya Ghosh <soumyaidmail@gmail.com>
2022-08-18 16:55:14 +0530

    leetcode problems

---
+++ find_sequence_of_number.js
@@ -0,0 +1,6 @@
+/*
+Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+array123([1, 1, 2, 3, 1]) → True
+array123([1, 1, 2, 4, 1]) → False
+array123([1, 1, 2, 1, 2, 3]) → True
+*/
