dcf1fe474dc4aa1b0d6aa3e693bdce12e075c0e8
Soumya Ghosh <soumyaidmail@gmail.com>
2022-09-09 23:05:40 +0530

    print sub sequence

--- Recursion/multiple_recursion.js
+++ Recursion/multiple_recursion.js
@@ -7,3 +7,22 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(4))
+
+//PRINTING SUB SEQUENCES
+
+function subSequence(i, arr) {
+    seq = [3, 2, 1]
+
+    if (i >= seq.length) {
+        console.log(arr)
+        return
+    }
+    //take
+    arr.push(seq[i])
+    subSequence(i + 1, arr)
+    //not take
+    arr.pop()
+    subSequence(i + 1, arr)
+    
+}
+subSequence(0, [])
