b48635151f0403e08b5b02cc6c2ce6b6b6f4f1d1
Soumya Ghosh <soumyaidmail@gmail.com>
2022-09-15 22:37:20 +0530

    n queen

--- leetcode/51_n_queens.js
+++ leetcode/51_n_queens.js
@@ -1,3 +1,4 @@
+
 function dfs(col, board, result) {
     if (col === board.length) {
         let finalBoard = [...construct(board)]
@@ -6,9 +7,10 @@ function dfs(col, board, result) {
     }
     for (let row = 0; row < board.length; row++) {
         if (isPossible(row, col, board)) {
-            board[row][col] = 'Q'
-            dfs(col + 1, board, result)
-            board[row][col] = '.'
+            board[row][col] = 'Q';
+            console.log(board)
+            dfs(col + 1, board, result);
+            board[row][col] = '.';
         }
     }
 }
@@ -21,31 +23,39 @@ function construct(board) {
     return res;
 }
 function isPossible(row, col, board) {
-    const rowCopy = JSON.parse( JSON.stringify(row) );
-    const colCopy = JSON.parse( JSON.stringify(col) );
+    const rowCopy = JSON.parse(JSON.stringify(row))
+    const colCopy = JSON.parse( JSON.stringify(col))
     while (row >= 0 && col >= 0) {
-        if (board[row][col] == 'Q') return false;
-        row--;
-        col--;
+        if (board[row][col] === 'Q') {
+            return false
+        }
+        row--
+        col--
     }
     row = rowCopy;
     col = colCopy;
     while (col >= 0) {
-        if (board[row][col] == 'Q') return false;
+        if (board[row][col] === 'Q') {
+            return false;
+        }
         col--;
     }
 
     row = rowCopy;
     col = colCopy;
     while (col >= 0 && row < board.length) {
-        if (board[row][col] == 'Q') return false;
+        if (board[row][col] === 'Q') {
+            return false;
+        }
         col--;
         row++;
     }
+    row = rowCopy;
+    col = colCopy;
     return true;
 }
 let n = 4
-let result = []
+let result = new Array()
 let board = new Array(n).fill(new Array(n).fill('.'))
 dfs(0, board, result)
 console.log(result)
