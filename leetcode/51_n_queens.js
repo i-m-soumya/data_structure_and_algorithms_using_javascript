4aceead40abd48a9a20ba467b203e62f19ddfc82
Soumya Ghosh <soumyaidmail@gmail.com>
2022-09-15 23:08:28 +0530

    n queen

--- leetcode/51_n_queens.js
+++ leetcode/51_n_queens.js
@@ -1,30 +1,29 @@
 
 function dfs(col, board, result) {
     if (col === board.length) {
-        let finalBoard = [...construct(board)]
-        result.push(finalBoard)
+        let strArray = []
+        for (let i = 0; i < board.length; i++) {
+            let tempStr = ""
+            for (let j = 0; j < board.length; j++) {
+                tempStr += board[i][j]
+            }
+            strArray.push(tempStr)
+        }
+        result.push(strArray)
         return
     }
     for (let row = 0; row < board.length; row++) {
         if (isPossible(row, col, board)) {
-            board[row][col] = 'Q';
-            console.log(board)
-            dfs(col + 1, board, result);
-            board[row][col] = '.';
+            board[row][col] = 'Q'
+            dfs(col + 1, board, result)
+            board[row][col] = '.'
         }
     }
 }
-function construct(board) {
-    let res = [];
-    for (let i = 0; i < board.length; i++) {
-        let s = JSON.parse( JSON.stringify(board[i].toString()));
-        res.push(s);
-    }
-    return res;
-}
+
 function isPossible(row, col, board) {
     const rowCopy = JSON.parse(JSON.stringify(row))
-    const colCopy = JSON.parse( JSON.stringify(col))
+    const colCopy = JSON.parse(JSON.stringify(col))
     while (row >= 0 && col >= 0) {
         if (board[row][col] === 'Q') {
             return false
@@ -32,30 +31,31 @@ function isPossible(row, col, board) {
         row--
         col--
     }
-    row = rowCopy;
-    col = colCopy;
+    row = rowCopy
+    col = colCopy
     while (col >= 0) {
         if (board[row][col] === 'Q') {
-            return false;
+            return false
         }
-        col--;
+        col--
     }
 
-    row = rowCopy;
-    col = colCopy;
+    row = rowCopy
+    col = colCopy
     while (col >= 0 && row < board.length) {
         if (board[row][col] === 'Q') {
-            return false;
+            return false
         }
-        col--;
-        row++;
+        col--
+        row++
     }
-    row = rowCopy;
-    col = colCopy;
-    return true;
+    row = rowCopy
+    col = colCopy
+    return true
 }
+
 let n = 4
 let result = new Array()
-let board = new Array(n).fill(new Array(n).fill('.'))
+let board = Array.from({ length: n }, () => new Array(n).fill('.'))
 dfs(0, board, result)
 console.log(result)
