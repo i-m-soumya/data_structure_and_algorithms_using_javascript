b168820a451c43101661f74d383cf47dd02d47a8
Soumya Ghosh <soumyaidmail@gmail.com>
2022-10-23 11:13:37 +0530

    climbing stairs recursion

--- leetcode/70_climb_stairs.js
+++ leetcode/70_climb_stairs.js
@@ -9,3 +9,31 @@ var climbStairs = function(n) {
     }
     return waysToReach[n]
 };
+
+
+// USING RECURSION
+var climbStairs2 = function(n) {
+    let result = []
+    dfs(4, 0, [], result)
+    console.log(result)
+};
+
+function dfs(target, base, stack, result) {
+    if(target === base) {
+        let finalStack = [...stack]
+        result.push(finalStack)
+        return true
+    } 
+    if (target < base) {
+        return false
+    }
+    base = base + 1
+    stack.push(1)
+    dfs(target, base, stack, result)
+    base = base - 1 + 2
+    stack.pop()
+    stack.push(2)
+    dfs(target, base, stack, result)
+    base = base - 2
+    stack.pop()
+}
