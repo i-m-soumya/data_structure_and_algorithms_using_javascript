70055a94af57ae0e678cdb2cde8f9bd5086042a4
Soumya Ghosh <soumyaidmail@gmail.com>
2022-12-05 22:45:49 +0530

    Leetcode 876. Middle of the Linked List

---
+++ leetcode/876_middle_of_the_linked_list.js
@@ -0,0 +1,8 @@
+var middleNode = function(head) {
+    let fast = slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
